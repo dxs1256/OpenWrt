d1be2d744bf1c701a248b35bdbc27d245018f41d
dxs1256 <dxs1256@gmail.com>
2024-02-19 17:28:09 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -31,7 +31,7 @@ git clone https://github.com/dxs12566/nas-packages.git package/luci-app-unishare
 # Themes
 # rm -rf feeds/luci/themes/luci-theme-argon
 # rm -rf feeds/luci/applications/luci-app-argon-config # if have
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/applications/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
 # 修改本地时间格式
