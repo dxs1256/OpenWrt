02a37ca61b960f97e32f0c2663aa2b82bbc9eb4e
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2025-09-18 07:50:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-opentomcat
 rm -rf feeds/luci/applications/luci-app-ksmbd
 rm -rf feeds/luci/applications/luci-app-opkg
 
@@ -32,7 +31,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git_sparse_clone main https://github.com/haiibo/packages package/luci-theme-opentomcat
+git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
 
 
 # 修改本地时间格式
