2ccdddb0aece8881a373c50740e224bc1859320c
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2025-09-17 21:16:04 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-set -e
-
-# 函数：增量拉取插件
-function ensure_package() {
-  pkg_name="$1"
-  repo_url="$2"
-  dest_dir="package/$pkg_name"
-
-  if [ ! -d "$dest_dir" ]; then
-    git clone --depth 1 "$repo_url" "$dest_dir"
-  else
-    echo "$pkg_name 已存在，尝试更新..."
-    cd "$dest_dir"
-    git pull --rebase --depth 1 || true
-    cd ../../
-  fi
-}
 
 # 删除 feed 里自带的插件
 rm -rf feeds/luci/themes/luci-theme-argon
@@ -23,6 +6,16 @@ rm -rf feeds/luci/applications/luci-app-pushbot
 rm -rf feeds/luci/applications/luci-app-ksmbd
 rm -rf feeds/luci/applications/luci-app-opkg
 rm -rf feeds/luci/applications/luci-app-unishare
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
 # 拉取自定义插件（增量）
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
@@ -33,13 +26,11 @@ git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 # 主题
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
-
-# 修改版本为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by Situ/g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 Makefile
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
@@ -53,6 +44,5 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 # 添加外部软件包源
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 更新并安装所有源
 ./scripts/feeds update -a
 ./scripts/feeds install -a
