abd852afea91dc44061145e76c2fab0feef7cafa
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2025-09-27 17:12:29 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -29,10 +29,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 # 修改本地时间显示格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm || true
 
-# 修改版本号为编译日期
-date_version=$(date +"%y.%m.%d")
-orig_version=$(grep "DISTRIB_REVISION=" package/lean/default-settings/files/zzz-default-settings | awk -F"'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by Situ/g" package/lean/default-settings/files/zzz-default-settings || true
+
 
 # 修复 Makefile 路径
 find package/*/ -maxdepth 2 -name "Makefile" | xargs -r -I {} sed -i 's|\.\./\.\./luci.mk|$(TOPDIR)/feeds/luci/luci.mk|g' {}
